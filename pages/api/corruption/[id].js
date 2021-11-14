74bd75ed58ed71e72d633b2a3ced3bbad002252f
dolong <long@mirrorme3d.com>
2021-11-14 12:56:06 -0500

    fix folder str

--- pages/api/corruption/[id].js
+++ pages/api/corruption/[id].js
@@ -1,5 +1,5 @@
 
-var Corruption = require('./src/index.js');
+var Corruption = require('../src/index.js');
 
 export default async (req, res) => {
 
@@ -11,10 +11,9 @@ export default async (req, res) => {
   
   switch (method) {
     case 'GET':
-      // Get data from your database
       let corruption = new Corruption("https://mainnet.infura.io/v3/752f065f467246a686ba54b11f77dcfd");
-      var bag = await corruption.bag(id);
-      res.end(JSON.stringify(bag))
+      var attributes = await corruption.attributes(id);
+      res.end(JSON.stringify(attributes))
       break
     default:
       res.setHeader('Allow', ['GET', 'PUT'])
