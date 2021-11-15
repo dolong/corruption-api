2a7e0f2606195f7ea76654b5770bb4de151c32c7
dolong <long@mirrorme3d.com>
2021-11-14 22:45:15 -0500

    build error for bracket

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,8 @@ export default function Home() {
           Try out querying a corruption NFT
         </p>
         <p>
-          <a href="api/corruption/1">&#123;api-url&#125;/corruption/{id}</a></p>
+          <a href="api/corruption/1">&#123;api-url&#125;/corruption/[id]</a></p>
+
 
         <div className={styles.grid}>
           <a href="https://thegraph.com/hosted-service/subgraph/shahruz/corruptions?version=current" className={styles.card}>
