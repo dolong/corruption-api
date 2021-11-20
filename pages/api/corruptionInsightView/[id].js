899bdc5c43040058d86dcff031e35c8d446e6d90
Long <ldo@vhslab.com>
2021-11-20 08:47:26 -0500

    Throttle

--- pages/api/corruptionInsightView/[id].js
+++ pages/api/corruptionInsightView/[id].js
@@ -2,16 +2,25 @@
 var Corruption = require('../metasrc/index.js');
 
 export default async (req, res) => {
-
+  
   const {
     query: { id },
     method,
   } = req
   
   
-  let corruption = new Corruption("https://mainnet.infura.io/v3/dc503cd8a1f249a1a6500d0f5f331eca");
-  var tokenURI = await corruption.tokenURI(id);
+  let id = corruptionId.split("id=")[1]
+  id = id.match(r)
 
-  res.end((`<div style="width: 500px;">${tokenURI}</div>`))
+  let insight = corruptionId.split("insight=")[1]
+  insight = insight.match(r)
+  let insightInt = parseInt(insight) 
 
+  if (insightInt > 20)
+    res.end((`<div style="width: 500px;">No mutations after 20 for now</div>`))
+  else {
+    let corruption = new Corruption("https://mainnet.infura.io/v3/dc503cd8a1f249a1a6500d0f5f331eca");
+    var tokenURI = await corruption.tokenURI(id);
+    res.end((`<div style="width: 500px;">${tokenURI}</div>`))
+  }
 }
