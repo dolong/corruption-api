48572722d8e4e648d6d3bb1145abb69175e8cfed
Long <ldo@vhslab.com>
2021-11-20 08:54:21 -0500

    Fix const

--- pages/api/corruptionInsightView/[id].js
+++ pages/api/corruptionInsightView/[id].js
@@ -8,18 +8,19 @@ export default async (req, res) => {
     method,
   } = req
   
+  const r = /\d+/;
   
-  let cid = corruptionId.split("id=")[1]
+  let cid = id.split("id=")[1]
   cid = cid.match(r)
 
-  let insight = corruptionId.split("insight=")[1]
+  let insight = id.split("insight=")[1]
   insight = insight.match(r)
   let insightInt = parseInt(insight) 
 
   if (insightInt > 20)
     res.end((`<div style="width: 500px;">No mutations after 20 for now</div>`))
   else {
-    let corruption = new Corruption("https://mainnet.infura.io/v3/dc503cd8a1f249a1a6500d0f5f331eca");
+    let corruption = new Corruption();
     var tokenURI = await corruption.tokenURI(id);
     res.end((`<div style="width: 500px;">${tokenURI}</div>`))
   }
