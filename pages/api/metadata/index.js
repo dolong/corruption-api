ff1d5d460904d087e705efd7efbdd57806285616
0xdecaff.eth <0xdecaff@protonmail.com>
2021-11-21 15:08:07 -0700

    patch

--- pages/api/metadata/index.js
+++ pages/api/metadata/index.js
@@ -8,22 +8,26 @@ export default async (req, res) => {
     method,
   } = req
 
-  switch (method) {
-    case 'GET':
-      if (query["start"] === undefined  || query["end"] === undefined) {
-        res.status(400).end(JSON.stringify({error: 'Bad request: query params start & end required (ints)'}))
-        return;
-      }
-      let corruption = new Corruption("https://mainnet.infura.io/v3/dc503cd8a1f249a1a6500d0f5f331eca");
-      const attributes = await corruption.attributesNoInfura(query.start, query.end + 1);
-      res.end(JSON.stringify(attributes));
-      break;
-    case 'OPTIONS':
-      res.end(JSON.stringify({data: 'OK'}));
-      break;
-    default:
-      res.setHeader('Allow', ['GET', 'OPTIONS'])
-      res.status(405).end(`Method ${method} Not Allowed`)
+  try {
+    switch (method) {
+      case 'GET':
+        if (query["start"] === undefined  || query["end"] === undefined) {
+          res.status(400).end(JSON.stringify({error: 'Bad request: query params start & end required (ints)'}))
+          return;
+        }
+        let corruption = new Corruption("https://mainnet.infura.io/v3/dc503cd8a1f249a1a6500d0f5f331eca");
+        const attributes = await corruption.attributesNoInfura(parseInt(query.start), parseInt(query.end) + 1);
+        res.end(JSON.stringify(attributes));
+        break;
+      case 'OPTIONS':
+        res.end(JSON.stringify({data: 'OK'}));
+        break;
+      default:
+        res.setHeader('Allow', ['GET', 'OPTIONS'])
+        res.status(405).end(`Method ${method} Not Allowed`)
+    }
+  } catch(err) {
+    res.end(500).end(`error: ${query} ${err}`)
   }
 
 }
